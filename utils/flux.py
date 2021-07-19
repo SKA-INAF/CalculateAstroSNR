f2397bcff5e0857a99da5ebabed0029833781f6e
rensortino <rensortino@gmail.com>
2021-07-19 10:26:08 +0200

    Added integrated flux computation

--- utils/flux.py
+++ utils/flux.py
@@ -7,3 +7,15 @@ import numpy as np                  # for numpy arrays and other operations
 def compute_peak_flux(masked_image: np.ma.MaskedArray) -> float:
     objects: np.ma.MaskedArray = masked_image[masked_image.mask]
     return objects.data.max()
+
+
+def compute_integrated_flux(masked_image: np.ma.MaskedArray, header: np.array) -> float:
+    objects: np.ma.MaskedArray = masked_image[masked_image.mask]
+    flux_sum = objects.sum()
+    cdelt1 = header['CDELT1']
+    cdelt2 = header['CDELT2']
+    bmaj = header['BMAJ']
+    bmin = header['BMIN']
+    beam_area = (cdelt1 * cdelt2) / (bmaj * bmin)
+    integrated_flux = flux_sum / beam_area
+    return integrated_flux

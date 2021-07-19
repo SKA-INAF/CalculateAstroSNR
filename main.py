69af485474e4e7ee9c1eaf2ef700e898c5033364
rensortino <rensortino@gmail.com>
2021-07-19 11:13:46 +0200

    Added SNR computation and output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2021  Daniel Magro and Renato Sortino
 # Full License at: https://github.com/DanielMagro97/CalculateAstroSNR/blob/main/LICENSE
 
-from typing import List             # for type annotation
+from typing import List, Dict       # for type annotation
 
 import argparse                     # for parsing command line arguments
 import os                           # for working with files and directories
@@ -10,7 +10,7 @@ from tqdm import tqdm               # for progress bars
 import numpy as np                  # for numpy arrays and other operations
 import astropy.stats                # for 3-sigma clipping and MAD
 
-from utils.load_fits import load_fits_image, read_samples     # for loading fits images with optional normalisation
+from utils.data import get_output_path, load_fits_image, read_samples, save_to_json     # for loading fits images with optional normalisation
 from utils.flux import compute_peak_flux
 
 # Suppress `Invalid 'BLANK' keyword in header.` warnings
@@ -32,6 +32,9 @@ def main(args):
     image_json_list_path = Path(args.json_list_path)
 
     samples: List = read_samples(image_json_list_path)
+
+    images_to_snr_3sigma : Dict = {}
+    images_to_snr_mad : Dict = {}
 
     # Look at each JSON file in image_json_path_list
     for sample in tqdm(samples, desc='Going through images and their object masks, and calculating MAD'):
@@ -71,7 +74,6 @@ def main(args):
             # load the mask and headers from disk
             fits_mask = load_fits_image(mask_path)
             mask: np.ndarray = fits_mask[0].data
-            mask_header = fits_mask[0].header
 
             # convert the masks into boolean (for logical operations)
             boolean_mask: np.ndarray = (mask != 0)
@@ -91,6 +93,17 @@ def main(args):
         peak_flux: float = compute_peak_flux(masked_image)
         print(f'Peak Flux: {peak_flux}')
 
+        snr_3sigma = peak_flux / three_sigma_clip
+        snr_mad = peak_flux / mad
+
+        # Add entry for each image path
+        img_path = get_output_path(sample)
+        images_to_snr_3sigma[img_path] = snr_3sigma
+        images_to_snr_mad[img_path] = snr_mad
+
+    # Save our dictionaries to JSON
+    save_to_json(images_to_snr_3sigma, 'images_to_snr_3sigma.json')
+    save_to_json(images_to_snr_mad, 'images_to_snr_mad.json')
 
 if __name__ == '__main__':
     args = get_args_parser().parse_args()
--- utils/data.py
+++ utils/data.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2021  Daniel Magro and Renato Sortino
 # Full License at: https://github.com/DanielMagro97/CalculateAstroSNR/blob/main/LICENSE
 
-from typing import List             # for type annotation
+from typing import Dict, List             # for type annotation
 
 import os                           # for working with paths
-import json                         # for reading JSON files
+import json
+from astropy.extern.configobj.validate import numToDottedQuad                         # for reading JSON files
 from tqdm import tqdm               # for progress bars
 import numpy as np                  # for numpy arrays
 from astropy.io import fits         # for opening .fits files
@@ -39,3 +40,31 @@ def read_samples(trainset_path) -> List:
                 image_mask_paths.append(label)
 
     return image_mask_paths
+
+
+class DataEncoder(json.JSONEncoder):
+    '''
+    Class to encode numpy arrays and floats into JSON
+    '''
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, np.float32):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
+def get_output_path(sample: Dict) -> str:
+    '''
+    Get the absolute image path
+    '''
+    img_path_list = sample['img'].split(os.sep)
+    start_dir = img_path_list.index('MLDataset_cleaned')
+    img_path_list = img_path_list[start_dir:]
+    return os.path.join(*img_path_list)
+
+def save_to_json(data: Dict, filename: str):
+    '''
+    Saves the dictionary data to JSON
+    '''
+    with open(filename, 'w') as out:
+        json.dump(data, out, cls=DataEncoder)
